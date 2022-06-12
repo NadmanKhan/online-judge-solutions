faff3349f8dc463791c37e449b6936348666adbb
Nadman A. Khan <nadman.ash.khan@gmail.com>
2022-06-13 00:15:48 +0600

    Fixed names and comments

--- UVA/10192 - Vacation/solution.cpp
+++ UVA/10192 - Vacation/solution.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 constexpr int len_max = 100; // Max string length in any test
 
-string a; // First of the string pairs
-string b; // Second of the string paralysis
+string a; // First of the input string pairs
+string b; // Second of the input string pairs
 int dp[len_max][len_max];
 
-int lcs(int i = 0, int j = 0) {
+int lcs_len(int i = 0, int j = 0) {
     if (i == a.size() || j == b.size()) {
         return 0;
     }
@@ -19,10 +19,9 @@ int lcs(int i = 0, int j = 0) {
     }
     int result = 0;
     if (a[i] == b[j]) {
-        result = 1 + lcs(i + 1, j + 1);
+        result = 1 + lcs_len(i + 1, j + 1);
     } else {
-        result = max(lcs(i + 1, j),
-                     lcs(i, j + 1));
+        result = max(lcs_len(i + 1, j), lcs_len(i, j + 1));
     }
 
     dp[i][j] = result;
@@ -37,7 +36,7 @@ void runcase(int casen) {
 
     // ** Solve **
 
-    int ans = lcs();
+    int ans = lcs_len();
 
     // ** Output **
 
@@ -49,6 +48,7 @@ int main() {
     cin.tie(nullptr);
 
     // ** Input & test-case handling **
+
     for (int casen = 1; getline(cin, a); ++casen) {
         if (a == "#") break;
         getline(cin, b);
